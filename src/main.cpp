a9c24eb458472eb9f68f74388779cea7a63dbd05
ak2hr <ak2hr@virginia.edu>
2021-06-03 14:50:18 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
